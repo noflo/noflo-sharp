6cc809632fadc9d35d83b085b013115844e2e3fb
Vilson Vieira <vilson@void.cc>
2015-05-31 23:58:27 -0700

    Set ld lib path

--- install-libvips.sh
+++ install-libvips.sh
@@ -3,4 +3,4 @@ set -ex
 wget http://void.cc/libvips-precise64.tar.gz
 tar -xvzf libvips-precise64.tar.gz -C /usr/local
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-echo $PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/usr/local/lib
